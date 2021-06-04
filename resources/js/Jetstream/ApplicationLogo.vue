dbc38644f28fe3a7e1591fa9ced5e732175c0e63
Inggo <inggo.espinosa@gmail.com>
2021-06-04 23:33:39 +0800

    Start dev

---
+++ resources/js/Jetstream/ApplicationLogo.vue
@@ -0,0 +1,78 @@
+<template>
+  <svg
+    width="100%"
+    viewBox="0 0 1080 1080"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    class="block h-9 w-auto"
+  >
+    <path
+      d="M 0,280 V 880H 442.5
+           L 450,895
+           H 632.5
+           L 640,880
+           H 1080
+           V 280"
+      fill="#53333E"
+    ></path>
+    <path
+      d="M 40,260
+          H 1040
+          V 850
+          H 40
+          V 257.5"
+      stroke="#76666E"
+      stroke-width="5"
+      fill="#FFF"
+    ></path>
+    <path
+      d="M 80,240
+           A 280,120 0,0,1 540,240
+           A 280,120 0,0,1 1000,240
+           V 830
+           A 280,120 1,0,0 540,850
+           A 280,120 1,0,0 80,830
+           V 237.5
+           M 540,240
+           V 850"
+      stroke="#76666E"
+      stroke-width="5"
+      fill="#FFF"
+    ></path>
+    <g transform="rotate(-42.5 750 750) translate(10,140)">
+      <text
+        transform="scale(0.85,1.25)"
+        font-style="normal"
+        font-weight="normal"
+        text-anchor="start"
+        font-family="serif"
+        font-size="240"
+        x="1000"
+        y="600"
+        stroke-width="0"
+        fill="#75555E"
+        opacity="0.85"
+      >
+        I
+      </text>
+      <text
+        transform="scale(1.5,1)"
+        opacity="0.85"
+        font-style="normal"
+        font-weight="normal"
+        text-anchor="start"
+        font-family="'Montserrat', sans-serif"
+        font-size="240"
+        y="675"
+        x="512.5"
+        fill="#75555E"
+      >
+        S
+      </text>
+    </g>
+  </svg>
+</template>
+
+<style>
+
+</style>
